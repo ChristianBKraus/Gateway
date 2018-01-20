23987f6c0ef013b0533b39cf4997477319e7b98b
D043312 <D043312@WDFN33414359A.emea.global.corp.sap>
2018-01-20 12:05:38 +0100

    Logging Filter enabled (+comment out old security)

--- src/main/java/jupiterpa/gateway/security/controller/LoginException.java
+++ src/main/java/jupiterpa/gateway/security/controller/LoginException.java
@@ -1,13 +1,13 @@
-package jupiterpa.gateway.security.controller;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class LoginException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-	public LoginException(String text) {
-		super(text);
-	}
-}
+//package jupiterpa.gateway.security.controller;
+//
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.http.HttpStatus;
+//
+//@ResponseStatus(HttpStatus.UNAUTHORIZED)
+//public class LoginException extends RuntimeException{
+//	private static final long serialVersionUID = 1L;
+//
+//	public LoginException(String text) {
+//		super(text);
+//	}
+//}
--- src/main/java/jupiterpa/gateway/security/model/JwtUserLogin.java
+++ src/main/java/jupiterpa/gateway/security/model/JwtUserLogin.java
@@ -1,45 +1,45 @@
-package jupiterpa.gateway.security.model;
-
-public class JwtUserLogin {
-    String userName;
-    String role;
-    String key;
-    
-    public JwtUserLogin() {}
-    
-    public JwtUserLogin(String userName, String role, String key) {
-    	this.userName = userName;
-    	this.role = role;
-    	this.key = key;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-	
-	@Override 
-	public String toString() {
-		return "Username=" + userName + " Role=" + role + " key=" + key;
-	}
-    
-}
+//package jupiterpa.gateway.security.model;
+//
+//public class JwtUserLogin {
+//    String userName;
+//    String role;
+//    String key;
+//    
+//    public JwtUserLogin() {}
+//    
+//    public JwtUserLogin(String userName, String role, String key) {
+//    	this.userName = userName;
+//    	this.role = role;
+//    	this.key = key;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//	public String getKey() {
+//		return key;
+//	}
+//
+//	public void setKey(String key) {
+//		this.key = key;
+//	}
+//	
+//	@Override 
+//	public String toString() {
+//		return "Username=" + userName + " Role=" + role + " key=" + key;
+//	}
+//    
+//}
